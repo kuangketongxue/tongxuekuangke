0da8657742970bf4cb1960a2e2c1cba6626ea818
kuangketongxue <19927675096@163.com>
2025-10-17 12:24:12 +0800

    Update leancloud-init.js

--- personal-website/shared/leancloud-init.js
+++ personal-website/shared/leancloud-init.js
@@ -1,1 +1,6 @@
-
+// 替换为你的 LeanCloud 信息（登录 LeanCloud 控制台 -> 应用 -> 设置 -> 应用凭证）
+AV.init({
+  appId: "2pmu0Y0IKEfIKXhdJHNEd1uU-gzGzoHsz",  // 例如：qwe123asd456zxc789
+  appKey: "cbLreTdVyxyXuWgmfwdQxPFF",  // 例如：123qwe456asd789zxc
+  serverURL: "https://2pmu0y0i.lc-cn-n1-shared.com"  // 例如：https://abc.leanapp.cn
+});
